8ea84bd6cf8a5314ab9801365d45fb86524dd699
berkay6nay <berkayaltinay09@gmail.com>
2023-04-11 18:08:32 +0300

    second commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import BlogListView,BlogDetailView
+from .views import BlogListView,PostCreateView,BlogDetailView,PostUpdateView,PostDeleteView
 
 urlpatterns = [
     path("" , BlogListView.as_view() , name = "home"),
-    path("post/<int:pk>/" , BlogDetailView.as_view() , name="post_detail")
+    path("post/new/" , PostCreateView.as_view() ,name="post_new") ,
+    path("post/<int:pk>/edit" , PostUpdateView.as_view() , name= "post_edit"),
+    path("post/<int:pk>/delete" , PostDeleteView.as_view() , name="post_delete"),
+    path("post/<int:pk>/" , BlogDetailView.as_view() , name="post_detail"),
     
-
-]
+    ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView ,DetailView
+from django.views.generic import ListView ,DetailView 
+from django.views.generic.edit import CreateView , UpdateView ,DeleteView
+from django.urls import reverse_lazy
 from .models import Post
 
 
@@ -10,4 +12,19 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = "post_detail.html"
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ["author" ,"title" ,"band_name" ,"album_name" ,"rating"]
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ["band_name" , "album_name" , "title" ,"rating"]
+    template_name = "post_edit.html"
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = reverse_lazy("home")
+    template_name = "post_delete.html"
 # Create your views here.
